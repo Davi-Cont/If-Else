a39e0f2b8a5ac26811fc78ca14d6f79f0107506b
Davi Conti <davi.alvesskt@gmail.com>
2021-12-08 21:00:36 -0300

    If Else

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe seu peso:");
+            double peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe sua altura:");
+            double altura = double.Parse(Console.ReadLine());
+            
+            double imc = peso / (altura * altura);
+
+            if (imc < 22.0)
+            {
+                Console.WriteLine("Magreza" +
+                                    "\n seu IMC é de: " + imc);
+            }
+
+            else if (imc > 27.0)
+            {
+                Console.WriteLine("Execesso de peso" +
+                                    "\n seu IMC é de: " + imc);
+            }
+
+            else
+            {
+                Console.WriteLine("Eutrofia (Peso adequado)" +
+                                    "\n seu IMC é de: " + imc);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
